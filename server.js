eb83d87ace12c29e29faa5797deec6c7df273263
kofi arhin <kofiarhin69@gmail.com>
2025-09-16 03:19:15 +0100

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.port || 5000;
+
+app.get("/", (req, res, next) => {
+  return res.json({ message: "hello world" });
+});
+
+app.listen(port, () => console.log("server started on port:", port));
