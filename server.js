ede320745fa818126e0aa27f898de693e76eea35
kofi arhin <kofiarhin69@gmail.com>
2025-09-16 03:39:14 +0100

    added email sending functionality using nodemailer

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const sendEmail = require("./sendEmail");
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res, next) => {
+app.get("/", async (req, res, next) => {
+  const options = {
+    to: "kofiarhin69@gmail.com",
+    subject: "tesing mic",
+    text: "this is a testing text",
+  };
+
+  await sendEmail(options);
   return res.json({ message: "hello world" });
 });
 
